7f5c880778078033caac3821fd947b7593147297
howardnguyen714 <howardnguyen714@gmail.com>
2021-02-01 14:36:11 -0800

    moveIntersect function made

--- js/app.js
+++ js/app.js
@@ -1,23 +1,50 @@
 console.log("Sanity Check!")
 
 let pokemonRequestUrl = "https://pokeapi.co/api/v2/pokemon/"
-let moveRequestUrl = ""
+let moveTypeRequestUrl = "https://pokeapi.co/api/v2/type/"
+let pokemonObject = {}
+let moveTypeObject = {}
+
 form.addEventListener("submit", (evt) => {
     evt.preventDefault()
-    let pokemon = document.querySelector("#pokemonInput").value
-    console.log(pokemonRequestUrl+pokemon)
+    let pokemon = document.querySelector("#pokemonInput")
+    console.log(pokemonRequestUrl+pokemon.value)
     // make the fetch request with the input
-    fetch(pokemonRequestUrl + pokemon)
+    fetch(pokemonRequestUrl + pokemon.value)
         .then((responseData) => {
             return responseData.json()
         })
         .then((jsonData) => {
-            console.log("this is the data", jsonData)
+            console.log("this is the pokemon data", jsonData)
+            pokemonObject = jsonData
         })
         .catch((error) => {
             console.error("ERROR: ", error)
         })
     
-    let move = document.querySelector("#moveInput").value
-    console.log(move)
+    let moveType = document.querySelector("#moveInput")
+    console.log(moveTypeRequestUrl+moveType.value)
+    fetch(moveTypeRequestUrl+moveType.value)
+        .then((responseData) => {
+            return responseData.json()
+        })
+        .then((jsonData) => {
+            console.log("this is the move type data", jsonData)
+            moveTypeObject = jsonData
+        })
+        .catch((error) => {
+            console.error("ERROR: ", error)
+        })
 })
+
+function moveIntersect () {
+    for (pokemonMoveItem in pokemonObject.moves) {
+        let pokemonMove = pokemonObject.moves[pokemonMoveItem].move.name
+        for (typeMoveItem in moveTypeObject.moves) {
+            let typeMove = moveTypeObject.moves[typeMoveItem].name
+            if (pokemonMove === typeMove) {
+                console.log(typeMove)
+            }
+        }
+    }
+}
