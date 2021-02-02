7bf3adc1bd9b88dcdebba9c7609e3a7f876987f1
howardnguyen714 <howardnguyen714@gmail.com>
2021-02-01 17:04:27 -0800

    dom manipulation stuff. Table generates

--- js/app.js
+++ js/app.js
@@ -2,14 +2,33 @@ console.log("Sanity Check!")
 
 let pokemonRequestUrl = "https://pokeapi.co/api/v2/pokemon/"
 let moveTypeRequestUrl = "https://pokeapi.co/api/v2/type/"
+let moveRequestUrl = "https://pokeapi.co/api/v2/move/"
 let pokemonObject = {}
 let moveTypeObject = {}
+let moveObject = {}
+let intersectArray = []
 
-form.addEventListener("submit", (evt) => {
-    evt.preventDefault()
+
+function moveIntersect () {
+    intersectArray = []
+    for (pokemonMoveItem in pokemonObject.moves) {
+        let pokemonMove = pokemonObject.moves[pokemonMoveItem].move.name
+        for (typeMoveItem in moveTypeObject.moves) {
+            let typeMove = moveTypeObject.moves[typeMoveItem].name
+            if (pokemonMove === typeMove) {
+                intersectArray.push(typeMove)
+            }
+        }
+    }
+    console.log("moveIntersect has been called")
+    console.log("intersectArray:", intersectArray)
+}
+
+function matchMoves () {
     let pokemon = document.querySelector("#pokemonInput")
     console.log(pokemonRequestUrl+pokemon.value)
-    // make the fetch request with the input
+    
+    // make fetch request for pokemon
     fetch(pokemonRequestUrl + pokemon.value)
         .then((responseData) => {
             return responseData.json()
@@ -22,8 +41,11 @@ form.addEventListener("submit", (evt) => {
             console.error("ERROR: ", error)
         })
     
+    
     let moveType = document.querySelector("#moveInput")
     console.log(moveTypeRequestUrl+moveType.value)
+    
+    // make fetch request for move type
     fetch(moveTypeRequestUrl+moveType.value)
         .then((responseData) => {
             return responseData.json()
@@ -35,16 +57,48 @@ form.addEventListener("submit", (evt) => {
         .catch((error) => {
             console.error("ERROR: ", error)
         })
-})
+}
 
-function moveIntersect () {
-    for (pokemonMoveItem in pokemonObject.moves) {
-        let pokemonMove = pokemonObject.moves[pokemonMoveItem].move.name
-        for (typeMoveItem in moveTypeObject.moves) {
-            let typeMove = moveTypeObject.moves[typeMoveItem].name
-            if (pokemonMove === typeMove) {
-                console.log(typeMove)
-            }
-        }
+function makeTable () {
+    console.log("makeTable has been called")
+    for (move in intersectArray) {
+        let tr = document.createElement("tr")
+        tbody.append(tr)
+        let tdName = document.createElement("td")
+        tr.appendChild(tdName)
+        tdName.textContent = intersectArray[move]
+        
+        fetch(moveRequestUrl + intersectArray[move])
+            .then((responseData) => {
+                return responseData.json()
+            })
+            .then((jsonData) => {
+                console.log("this is the move data", jsonData)
+                moveObject = jsonData
+            })
+            .catch((error) => {
+                console.error("ERROR: ", error)
+            })
+    
+        let tdPow = document.createElement("td")
+        tr.append(tdPow)
+        tdPow.innerText = moveObject.power
+        
     }
 }
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault()
+    
+    matchMoves()
+
+    setTimeout(moveIntersect,500)
+
+    let tbody = document.getElementById("tbody")
+
+    setTimeout(makeTable, 750)
+
+})
+
+
+
